efa7f6a8e3d1bf45bd66f812886b5b5a7b9f4003
Bas K <39069678+Basje12@users.noreply.github.com>
2020-02-25 00:11:11 +0100

    Remove docs

--- docs/styles-09e7c7d1b412c1751b47.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{FfUf:function(n,o,w){},"sg+I":function(n,o,w){}}]);
-//# sourceMappingURL=styles-09e7c7d1b412c1751b47.js.map
--- docs/styles-40274f5d2e532162afd6.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,o,p){},FfUf:function(n,o,p){},"sg+I":function(n,o,p){}}]);
-//# sourceMappingURL=styles-40274f5d2e532162afd6.js.map
