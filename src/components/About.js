84ff0678cbf16c540823451213eb99286507db93
Bas K <39069678+Basje12@users.noreply.github.com>
2020-02-28 15:59:10 +0100

    Typo in about

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,16 @@ export default function About() {
     <div className="about">
       <div className="about__text">
         <h2 className="heading">About Me</h2>
-        <p>The first website I ever built, at 12 years old, was of course on Geocities. The efforts were thoroughly enjoyable and, though fairly embarrassing, tied up the phoneline something fierce.</p>
-        <p>A lot has changed since then and I've dived back into learning web development. Including the back-end stuff this time. It's still as enjoyable as it was back then. There's just many more possibilities</p>
+        <p>
+          The first website I ever built, at 12 years old, was of course on
+          Geocities. The efforts were thoroughly enjoyable, though fairly
+          embarrassing, and tied up the phoneline something fierce.
+        </p>
+        <p>
+          A lot has changed since then and I've dived back into learning web
+          development. Including the back-end stuff this time. It's still as
+          enjoyable as it was back then. There's just many more possibilities
+        </p>
       </div>
       <div>
         <img src={profilePic} alt="Profielfoto Bas" className="about__image" />
